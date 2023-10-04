d36768d45061acc9a476ecb51a443181bd3e1843
UriahK <143620100+UriahK@users.noreply.github.com>
2023-10-04 09:23:53 -0400

    Add files via upload

---
+++ tree.R
@@ -0,0 +1,5 @@
+install.packages("ape"); install.packages("phangorn"); install.packages("phytools"); install.packages("geiger")
+library(ape)
+text.string<- "(((((((cow, pig),whale),(bat,(lemur,human))),(robin,iguana)),coelacanth),gold_fish),shark)"
+vert.tree<-read.tree(text=text.string)
+plot(vert.tree,no.margin=TRUE,edge.width=2)
